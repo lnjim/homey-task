5d5d1ef3c4be7e3f6cf55af4324f7812f086c493
Locman Diawadoh <locman@locman.fr>
2025-02-10 12:38:39 +0100

    rubocop executed

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,6 +1,8 @@
+# frozen_string_literal: true
+
 class ProjectsController < ApplicationController
-  before_action :set_project, only: %i[ show edit update destroy update_status ]
-  before_action :ensure_owner, only: %i[ edit update destroy ]
+  before_action :set_project, only: %i[show edit update destroy update_status]
+  before_action :ensure_owner, only: %i[edit update destroy]
 
   # GET /projects or /projects.json
   def index
@@ -8,8 +10,7 @@ class ProjectsController < ApplicationController
   end
 
   # GET /projects/1 or /projects/1.json
-  def show
-  end
+  def show; end
 
   # GET /projects/new
   def new
@@ -17,8 +18,7 @@ class ProjectsController < ApplicationController
   end
 
   # GET /projects/1/edit
-  def edit
-  end
+  def edit; end
 
   # POST /projects or /projects.json
   def create
@@ -27,7 +27,7 @@ class ProjectsController < ApplicationController
 
     respond_to do |format|
       if @project.save
-        format.html { redirect_to @project, notice: "Project was successfully created." }
+        format.html { redirect_to @project, notice: 'Project was successfully created.' }
         format.json { render :show, status: :created, location: @project }
       else
         format.html { render :new, status: :unprocessable_entity }
@@ -40,7 +40,7 @@ class ProjectsController < ApplicationController
   def update
     respond_to do |format|
       if @project.update(project_params)
-        format.html { redirect_to @project, notice: "Project was successfully updated." }
+        format.html { redirect_to @project, notice: 'Project was successfully updated.' }
         format.json { render :show, status: :ok, location: @project }
       else
         format.html { render :edit, status: :unprocessable_entity }
@@ -66,30 +66,31 @@ class ProjectsController < ApplicationController
     @project.destroy!
 
     respond_to do |format|
-      format.html { redirect_to projects_path, status: :see_other, notice: "Project was successfully destroyed." }
+      format.html { redirect_to projects_path, status: :see_other, notice: 'Project was successfully destroyed.' }
       format.json { head :no_content }
     end
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_project
-      @project = Project.find(params[:id])
-    end
 
-    # Only allow a list of trusted parameters through.
-    def project_params
-      params.require(:project).permit(:owner_id, :name, :status)
-    end
+  # Use callbacks to share common setup or constraints between actions.
+  def set_project
+    @project = Project.find(params[:id])
+  end
 
-    def status_params
-      params.require(:project).permit(:status)
-    end
+  # Only allow a list of trusted parameters through.
+  def project_params
+    params.require(:project).permit(:owner_id, :name, :status)
+  end
 
-    def ensure_owner
-      return if @project.owner == current_user
+  def status_params
+    params.require(:project).permit(:status)
+  end
 
-      flash[:error] = 'You are not the owner of this project.'
-      redirect_to projects_path, status: :forbidden
-    end
+  def ensure_owner
+    return if @project.owner == current_user
+
+    flash[:error] = 'You are not the owner of this project.'
+    redirect_to projects_path, status: :forbidden
+  end
 end
